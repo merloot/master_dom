1683b967f5135cdc234173a51f053e3294dd771f
merloot <Arkhipov@smartworld.team>
2019-10-01 20:16:45 +0700

    add Service

--- frontend/modules/doors/controllers/DefaultController.php
+++ frontend/modules/doors/controllers/DefaultController.php
@@ -4,6 +4,7 @@ namespace frontend\modules\doors\controllers;
 
 use frontend\modules\doors\models\Clients;
 use frontend\modules\doors\models\Doors;
+use frontend\modules\doors\models\ServiceDoors;
 use frontend\modules\doors\models\ServicePrice;
 use yii\web\Controller;
 
@@ -17,7 +18,11 @@ class DefaultController extends Controller {
     public function actionCreate() {
         $client = new Clients();
         $doors = new Doors();
+        $doorsService = new ServiceDoors();
         $service = ServicePrice::find()->all();
+        $serviceDemontag = ServiceDoors::find()->where(['type_service'=>ServicePrice::TYPE_SERVICE_DEMONTAG])->all();
+        $serviceBox = ServicePrice::find()->where(['type_service'=>ServicePrice::TYPE_SERVICE_BOXED_PRODUCT])->all();
+        $serviceOther = ServicePrice::find()->where(['type_service'=>ServicePrice::TYPE_SERVICE_OTHER])->all();
 
         if ($client->load(\Yii::$app->request->post()) && $client->save()) {
             if ($doors->load(\Yii::$app->request->post()) && $doors->save()){
@@ -25,9 +30,12 @@ class DefaultController extends Controller {
             return true;
         }
         return $this->render('create', [
-            'client'    => $client,
-            'doors'     => $doors,
-            'service'   => $service
+            'client'            => $client,
+            'doors'             => $doors,
+            'service'           => $service,
+            'box_product'       => $serviceBox,
+            'other'             => $serviceOther,
+            '$serviceDemontag'  => $serviceDemontag
         ]);
     }
 }
