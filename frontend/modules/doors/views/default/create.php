755241de2146d72d7f06f70d1ba09e3655ce0e7a
gGerz <34414584+gGerz@users.noreply.github.com>
2019-10-01 22:18:02 +0700

    frontend

--- frontend/modules/doors/views/default/create.php
+++ frontend/modules/doors/views/default/create.php
@@ -149,57 +149,40 @@ $this->title = 'Установка дверей';
                                                     <span id="priceCounter-2">2</span> Р
                                             </li>
                                         </ul>
-                                        <?= $form->field($doors, 'type_doors')->dropDownList([
-                                            Doors::TYPE_DOORS_IRON =>'Металическая',
-                                            Doors::TYPE_DOORS_INTERIOR =>'Межкомнатная'
-                                        ]) ?>
-
-                                        <?= $form->field($doors, 'comment')->textarea() ?>
 
                                         <hr>
 
-                                <div>
-                                    <label>Материал стен:</label>
-                                    <?= $form->field($doors, 'wall_material')->radioList([
-                                        'Сибит'     =>   'Сибит',
-                                        'Кирпич'    =>   'Кирпич',
-                                        'Ж/Бетон'   =>   'Ж/Бетон',
-                                        'Дерево'    =>   'Дерево',
-                                        'Другое'    =>   Html::textInput( 'wall_material',
-                                            Yii::$app->request->post('wall_material'),
-                                            [
-                                                'class' => 'form-control', 'placeholder' => 'Свой вариант'
-                                            ]),
-                                    ], [
-                                            'encode' => false
-                                    ])->label(false)
-                                    ?>
-                                </div>
+                                        <label>
+                                            Расходные материалы:
+                                        </label>
 
-                                        <hr>
-
-                                        <div>
-                                            <label>Вид проема в плане:</label>
-                                            <?=$form->field($doors, 'type_opening')->radioList([
-                                                Doors::TYPE_OPENING_MID   => Html::img('/image/mid_doors.svg', ['width' => '100%', 'height' => 150]) ,
-                                                Doors::TYPE_OPENING_LEFT  => Html::img('/image/left_doors_1.svg',['width' => '100%', 'height' => 150]) ,
-                                                Doors::TYPE_OPENING_RIGHT => Html::img('/image/right_doors.svg' ,['width' => '100%', 'height' => 150])
-                                            ], ['encode' => false])->label(false)
-                                            ?>
+                                        <div class="form-group services">
+                                            <select name="services" class="form-control">
+                                                <option value="1"></option>
+                                                <option value="1"></option>
+                                                <option value="1"></option>
+                                            </select>
+                                            <input class="form-control counter" type="number" placeholder="шт">
+                                            <button class="btn btn-master">+</button>
                                         </div>
 
-                                        <hr>
-
-                                        <div>
-                                            <label>Сторонность:</label>
-                                            <?=$form->field($doors, 'adherence')->radioList([
-                                                Doors::ADHERENCE_INTERIOR_LEFT      => Html::img('/image/right_doors_1.svg', ['width' => '100%', 'height' => 150]),
-                                                Doors::ADHERENCE_INTERIOR_RIGHT     => Html::img('/image/left_doors.svg',['width' => '100%', 'height' => 150]),
-                                                Doors::ADHERENCE_OUTDOOR_LEFT       => Html::img('/image/right_doors_1.svg', ['width' => '100%', 'height' => 150]),
-                                                Doors::ADHERENCE_OUTDOOR_RIGHT      => Html::img('/image/left_doors.svg',['width' => '100%', 'height' => 150]),
-                                            ], ['encode' => false])->label(false)
-                                            ?>
-                                        </div>
+                                        <ul class="list-group">
+                                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                                Первый материал
+                                                <span class="badge badge-primary badge-pill">
+                                                    <span id="materialCounter-1">14</span>
+                                                    X
+                                                    <span id="materialPriceCounter-1">500</span> Р
+                                                </span>
+                                            </li>
+                                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                                Второй материал
+                                                <span class="badge badge-primary badge-pill">
+                                                    <span id="materialCounter-2">3</span>
+                                                    X
+                                                    <span id="materialPriceCounter-2">2</span> Р
+                                            </li>
+                                        </ul>
                                     </div>
                                 </div>
                                 <?= Html::submitButton('Сохранить', ['class' => 'btn btn-master']) ?>
