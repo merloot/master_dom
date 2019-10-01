8b58f56216468af03833b168d786406bf6fa8978
merloot <Arkhipov@smartworld.team>
2019-10-01 21:28:23 +0700

    bug fix

--- frontend/modules/doors/controllers/DefaultController.php
+++ frontend/modules/doors/controllers/DefaultController.php
@@ -16,13 +16,13 @@ class DefaultController extends Controller {
     }
 
     public function actionCreate() {
-        $client = new Clients();
-        $doors = new Doors();
-        $doorsService = new ServiceDoors();
-        $service = ServicePrice::find()->all();
-        $serviceDemontag = ServiceDoors::find()->where(['type_service'=>ServicePrice::TYPE_SERVICE_DEMONTAG])->all();
-        $serviceBox = ServicePrice::find()->where(['type_service'=>ServicePrice::TYPE_SERVICE_BOXED_PRODUCT])->all();
-        $serviceOther = ServicePrice::find()->where(['type_service'=>ServicePrice::TYPE_SERVICE_OTHER])->all();
+        $client             = new Clients();
+        $doors              = new Doors();
+        $doorsService       = new ServiceDoors();
+        $service            = ServicePrice::find()->all();
+        $serviceDemontag    = ServicePrice::find()->where(['type_service'=>ServicePrice::TYPE_SERVICE_DEMONTAG])->all();
+        $serviceBox         = ServicePrice::find()->where(['type_service'=>ServicePrice::TYPE_SERVICE_BOXED_PRODUCT])->all();
+        $serviceOther       = ServicePrice::find()->where(['type_service'=>ServicePrice::TYPE_SERVICE_OTHER])->all();
 
         if ($client->load(\Yii::$app->request->post()) && $client->save()) {
             if ($doors->load(\Yii::$app->request->post()) && $doors->save()){
