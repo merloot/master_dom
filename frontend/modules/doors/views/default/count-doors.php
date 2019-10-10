37c66305b01e9624051e4c2305f07e4039feb9aa
gGerz <34414584+gGerz@users.noreply.github.com>
2019-10-10 11:13:50 +0700

    frontend

--- frontend/modules/doors/views/default/count-doors.php
+++ frontend/modules/doors/views/default/count-doors.php
@@ -1,9 +1,38 @@
 <?php
 
 //\Yii::$app->response->format = 'json';
+$this->title = 'Кол-во дверей';
 ?>
-<form method="post" action="<?=\yii\helpers\Url::to('create')?>">
-    <textarea name="count" id="count"></textarea>
-    <input type="submit" value="Send" />
-</form>
+<div class="container">
+    <div class="container-doors-counter">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 co1-md-12 col-lg-12">
+                <div class="container-doors-counter__block">
+                    <h3>Введите количество дверей:</h3>
+                    <form method="post" action="<?=\yii\helpers\Url::to('create')?>">
+                        <input name="count" id="count" class="form-control counter-doors" type="number">
+                        <input type="submit" value="Отправить" class="btn btn-master" />
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
+<style>
+
+    .container-doors-counter__block{
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .container-doors-counter form{
+        display: flex;
+    }
+
+    .counter-doors{
+        max-width: 350px;
+    }
+</style>
